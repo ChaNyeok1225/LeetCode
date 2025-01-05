e345a4322769e2c5108f032b82f59dcd5f126c10
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2025-01-05 16:55:59 +0900

    Time: 0 ms (100%), Space: 12.1 MB (65.47%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -5,31 +5,21 @@ using namespace std;
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int size = m + n;        
-        int* arr = new int[size];
-
-        int index1 = 0;
-        int index2 = 0;
-        int p = 0;
-        while(index1 < m && index2 < n) {
-            if(nums1[index1] < nums2[index2]) {
-                arr[p++] = nums1[index1++];
+        int index = m + n;        
+        
+        index--;
+        m--; n--; 
+        
+        while(m >= 0 && n >= 0) {
+            if(nums1[m] > nums2[n]) {
+                nums1[index--] = nums1[m--];
             } else {
-                arr[p++] = nums2[index2++];
+                nums1[index--] = nums2[n--];
             }
-        }    
-
-        while(index1 < m) {
-            arr[p++] = nums1[index1++];
         }
-        while(index2 < n) {
-            arr[p++] = nums2[index2++];
+        while(n >= 0) {
+            nums1[index--] = nums2[n--];
         }
 
-        for(int i = 0; i < size; i++)
-            nums1[i] = arr[i];
-
-        delete[] arr;
-        arr = nullptr;
     }
 };
